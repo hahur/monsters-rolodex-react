fb8a1685c4ae4391e69a2a4579d463ee88074c9b
hahur <suyash.saxena337@gmail.com>
2019-10-24 15:18:17 +0530

    Updates

---
+++ precache-manifest.b9238a988b115193aa7b08b72b3ed1b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54817d8d172452e092beef99ded494c2",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "27634b7aa151eb3b6ee5",
+    "url": "/monsters-rolodex-react/static/css/main.05011ec2.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex-react/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "27634b7aa151eb3b6ee5",
+    "url": "/monsters-rolodex-react/static/js/main.74f24552.chunk.js"
+  },
+  {
+    "revision": "3e27ccdb83b609130aad",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.ee1f9c8f.js"
+  }
+]);
